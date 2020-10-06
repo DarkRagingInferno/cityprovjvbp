d597f158d448de638e9900fe91fec78c3acbe3e1
John Poku <jvbpoku@hotmail.com>
2020-10-05 21:23:02 -0700

    troublshoot identity user

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using SportsPlus.Models;
 
 namespace SportsPlus.Data {
-    public class ApplicationDbContext : DbContext {
+    public class ApplicationDbContext : IdentityDbContext {
         public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) { }
 
         protected override void OnModelCreating (ModelBuilder builder) {
